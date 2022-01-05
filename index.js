f35f7ba04a9239e01da8bdcb0b729dc5d45cab21
cDanowski <christian.danowski-buhren@hs-bochum.de>
2022-01-05 11:39:38 +0100

    add more dependencies and improve startup and release process

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 var path = require('path');
 var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var oas3Tools = require('oas3-tools');
-var serverPort = 8080;
+var serverPort = process.env.PORT || 8090;
+
+var oas3Tools = require('oas3-tools');
+var serverPort = 8090;
 
 // swaggerRouter configuration
 var options = {
@@ -14,7 +20,30 @@ var options = {
 };
 
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
+
+var openApiApp = expressAppConfig.getApp();
+
+/*
+    workaround to support CORS and modify some express middleware parameters
+*/
+
+const app = express();
+
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
+
+const corsOptions = {
+  // exposedHeaders: 'Access-Control-Allow-Origin,Location,Connection,Content-Type,Date,Transfer-Encoding'
+  exposedHeaders: ['Access-Control-Allow-Origin','Location','Connection','Content-Type','Date','Transfer-Encoding','Origin','X-Requested-With', 'Accept'],
+  origin: "*"
+};
+
+// Add headers
+app.use(/.*/, cors(corsOptions));
+
+for (let i = 2; i < openApiApp._router.stack.length; i++) {
+    app._router.stack.push(openApiApp._router.stack[i])
+}
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
